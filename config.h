c5440226a22b700f3054584129e57999c80073dd
petermouses <peter.mousses@gmail.com>
2021-03-03 21:36:50 -0700

    Set dmenu to XF86Search; bmenu and pulseaudio also set

--- config.h
+++ config.h
@@ -64,11 +64,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *bmenu[]  = { "urxvt", "-e", "bmenu" };
+static const char *pavucontrol[]  = { "urxvt", "-e", "pavucontrol" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ 0,                   XF86XK_Search,      spawn,          {.v = dmenucmd } },
+	//{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,          	 	XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -101,6 +102,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0,                   XF86XK_Search,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = bmenu } },
+	{ MODKEY|ShiftMask,             XK_m,      spawn,          {.v = pavucontrol } },
 };
 
 /* button definitions */
